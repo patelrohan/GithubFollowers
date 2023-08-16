88ed7119cad97a0528cdfa033c2d9e61e451c0c8
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-16 08:58:35 -0400

    FavVC, SearchVC, GHFButton, GHFTextField
    Added VC and custom button and textfield. WIP

---
+++ GithubFollowers/GHFButton.swift
@@ -0,0 +1,40 @@
+//
+//  GHFButton.swift
+//  GithubFollowers
+//
+//  Created by Rohan on 8/16/23.
+//
+
+import UIKit
+
+class GHFButton: UIButton {
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+        
+    }
+    
+    // for storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
--- GithubFollowers/SceneDelegate.swift
+++ GithubFollowers/SceneDelegate.swift
@@ -17,10 +17,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+      
+     
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = createTabBarController()
         window?.makeKeyAndVisible()
+    }
+    
+    func createSearchNC() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesListNC() -> UINavigationController{
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+    
+    func createTabBarController() -> UITabBarController{
+        let tabBarController = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabBarController.viewControllers = [createSearchNC(), createFavoritesListNC()]
+        
+        if #available(iOS 15, *) {
+                    let appearance = UITabBarAppearance()
+                    appearance.configureWithOpaqueBackground()
+                    UITabBar.appearance().standardAppearance = appearance
+                    UITabBar.appearance().scrollEdgeAppearance = appearance
+                }
+        
+        return tabBarController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
