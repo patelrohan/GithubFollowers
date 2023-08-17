8ade8414038abf704d260b9133fcaf35fda61c07
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-16 23:59:35 -0400

    SearchVC UI

--- GithubFollowers/SearchVC.swift
+++ GithubFollowers/SearchVC.swift
@@ -9,13 +9,75 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = GHFTextField()
+    let getFollowersBtn = GHFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        configureLogoImageView()
+        configureUsernameTextField()
+        configureGetFollowersBtn()
+        dismissKeyboard()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    func configureLogoImageView(){
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    
+    
+    func configureUsernameTextField(){
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    func configureGetFollowersBtn(){
+        view.addSubview(getFollowersBtn)
+        
+        NSLayoutConstraint.activate([
+            getFollowersBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -48),
+            getFollowersBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            getFollowersBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            getFollowersBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
 
+    func dismissKeyboard(){
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
+    }
+    
     /*
     // MARK: - Navigation
 
