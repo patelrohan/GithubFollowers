5fa41aed354b87435732e2896326dec5738c3968
Rohan Patel <patelrohan@users.noreply.github.com>
2023-09-17 23:59:27 -0400

    ItemInfoView (WIP)

---
+++ GithubFollowers/Custom Views/Views/GHFItemInfoView.swift
@@ -0,0 +1,69 @@
+//
+//  GHFItemInfoView.swift
+//  GithubFollowers
+//
+//  Created by Rohan on 9/17/23.
+//
+
+import UIKit
+
+enum ItemInfoType{
+    case repos, gists, following, followers
+}
+
+class GHFItemInfoView: UIView {
+
+    let iconImageView   = UIImageView()
+    let titleLabel      = GHFTitleLabel(textAlignment: .left, fontSize: 14)
+    let countLabel      = GHFTitleLabel(textAlignment: .center, fontSize: 14)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        addSubview(iconImageView)
+        addSubview(titleLabel)
+        addSubview(countLabel)
+        
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.contentMode   = .scaleAspectFill
+        iconImageView.tintColor     = .label
+        
+        NSLayoutConstraint.activate([
+            iconImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            iconImageView.leadingAnchor.constraint(equalTo: self.leftAnchor),
+            iconImageView.widthAnchor.constraint(equalToConstant: 20),
+            iconImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: iconImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            countLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 4),
+            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            countLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    func set(itemInfoType: ItemInfoType){
+        switch itemInfoType {
+         case .repos:
+            break
+        case .gists:
+            break
+        case .following:
+            break
+        case .followers:
+            break
+        }
+    }
+        
+}
